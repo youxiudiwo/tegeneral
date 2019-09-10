1b7fa126fba4c52075afaf987b355582c6b78e10
zuoshao_glb <youxiudiwo@github.com>
2019-09-10 11:44:56 +0800

    项目整体架构0.001

---
+++ src/main/java/com/zuoshao/tegeneral/TegeneralApplication.java
@@ -0,0 +1,13 @@
+package com.zuoshao.tegeneral;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TegeneralApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TegeneralApplication.class, args);
+    }
+
+}
