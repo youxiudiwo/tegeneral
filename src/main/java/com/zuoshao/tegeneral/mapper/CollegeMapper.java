09ed76873824ee0e98557ff52a3561040887e067
zhongdaye <1507758248@qq.com>
2019-09-17 11:00:18 +0800

    项目架构0.003zhong（院系）

---
+++ src/main/java/com/zuoshao/tegeneral/mapper/CollegeMapper.java
@@ -0,0 +1,7 @@
+package com.zuoshao.tegeneral.mapper;
+
+import com.zuoshao.tegeneral.bean.College;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface CollegeMapper extends Mapper<College> {
+}
