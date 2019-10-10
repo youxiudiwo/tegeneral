2cbb125a19e6c3bdd8a8823f0f12dfd21a7e33b1
cqyhm <32033658@qq.com>
2019-10-10 17:16:39 +0800

    试卷的生成和提交试卷的分数计算并且存储在评价总表 V1.0

---
+++ src/main/java/com/zuoshao/tegeneral/controller/GeneralController.java
@@ -0,0 +1,78 @@
+package com.zuoshao.tegeneral.controller;
+
+import com.zuoshao.tegeneral.bean.beanexa.FractionSum;
+import com.zuoshao.tegeneral.service.GeneralService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @Author: luquanlin
+ * @Date: 2019/10/10 10:55
+ * @VERSION: 1.0
+ **/
+@Api(description = "试卷提交信息和分数管理接口",value = "")
+@Controller
+public class GeneralController {
+    @Autowired
+    private GeneralService generalService;
+
+    @ApiOperation(value="评教完成后储存信息，评价总表", notes="test: 1有正确返回0发生异常错误")
+    @ApiImplicitParams({
+            @ApiImplicitParam(paramType="query", name = "userid", value = "评分人ID", required = true, dataType = "int"),
+            @ApiImplicitParam(paramType="query", name = "userid2", value = "被评分人ID", required = true, dataType = "int"),
+            @ApiImplicitParam(paramType="query", name = "quid", value = "问卷编号", required = true, dataType = "int"),
+            @ApiImplicitParam(paramType="query", name = "opids", value = "选项编号数组", required = true, dataType = "int"),
+            @ApiImplicitParam(paramType="query", name = "inids", value = "题目编号数组", required = true, dataType = "int"),
+            @ApiImplicitParam(paramType="query", name = "batch", value = "批次ID", required = true, dataType = "int")
+   })
+    @RequestMapping("/insertGeneral")
+    @ResponseBody
+    public Map insertGeneral(int userid, int userid2, int quid, String opids, String inids, int batch){
+        String a[]=opids.split(",");//选项的ID数组
+        String t[]=inids.split(",");//题目的ID数组
+        Map result =new HashMap();
+        float sum = 0;//计算总分
+        for (int i=0;i<a.length;i++){
+            String options = a[i];//选项单独一个
+            String targets_id = t[i];//题目单独一个
+
+            //转换成int类型
+            int ixid = Integer.parseInt(options);
+            int osid = Integer.parseInt(targets_id);
+
+
+            FractionSum fractionSum = new FractionSum();
+            List<FractionSum> fractions = generalService.addFractions(ixid,osid);//查询此题目的此选项分数
+            sum = fractions.get(0).getFraction();//计算整张试卷分数
+            String fraction = String.valueOf(sum);
+            if (generalService.insertGeneral(userid,userid2,quid,ixid,osid,batch,fraction)){
+                result.put("test",1);
+            }else{
+                result.put("test",0);
+            }
+        }
+        return result;
+    }
+
+    //    生成试卷
+    @ApiOperation(value="生成试卷", notes="直接返回数据")
+    @ApiImplicitParam(paramType="query", name = "qid", value = "试卷ID", required = true, dataType = "int")
+    @ResponseBody
+    @RequestMapping("/selectOnlineEvaluation")
+    public Map selectOnlineEvaluation(int qid){
+        Map result=new HashMap();
+        List<Map<String, Object>> online = generalService.selectOnlineEvaluation(qid);
+        result.put("subdata::",online);
+        return result;
+    }
+}
---
+++ src/main/java/com/zuoshao/tegeneral/service/GeneralService.java
@@ -0,0 +1,22 @@
+package com.zuoshao.tegeneral.service;
+
+import com.zuoshao.tegeneral.bean.beanexa.FractionSum;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @Author: luquanlin
+ * @Date: 2019/10/10 10:21
+ * @VERSION: 1.0
+ **/
+public interface GeneralService {
+    //计算分数
+    List<FractionSum> addFractions(@Param("ixid") int ixid, @Param("osid") int osid);
+    //插入试卷信息
+    Boolean insertGeneral(@Param("userid")int userid,@Param("userid2")int userid2,@Param("quid")int quid,@Param("opid")int opid,@Param("inid")int inid,@Param("batch")int batch,@Param("fraction")String fraction);
+    //生成试卷
+    List<Map<String, Object>> selectOnlineEvaluation(@Param("qid")int qid);
+    List<Map<String, Object>> selectOnlineEvaluationOptions(@Param("qid")int qid);
+}
