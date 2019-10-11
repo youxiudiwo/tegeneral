0dd1df315b32fbf18516a91d5a16bc1f0a7428a7
suhang36 <1501911202@qq.com>
2019-10-11 11:50:18 +0800

    gengai

--- src/main/java/com/zuoshao/tegeneral/mapper/BatchMapper.java
+++ src/main/java/com/zuoshao/tegeneral/mapper/BatchMapper.java
@@ -1,7 +1,9 @@
 package com.zuoshao.tegeneral.mapper;
 
 import com.zuoshao.tegeneral.bean.Batch;
+import org.springframework.stereotype.Component;
 import tk.mybatis.mapper.common.Mapper;
 
+@Component
 public interface BatchMapper extends Mapper<Batch> {
 }
