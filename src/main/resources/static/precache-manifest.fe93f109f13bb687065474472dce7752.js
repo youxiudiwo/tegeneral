e1d70a5367446d993bac337c4928f75737ba1d85
suhang36 <1501911202@qq.com>
2019-10-15 15:42:16 +0800

    '第二次合并前端'

---
+++ src/main/resources/static/precache-manifest.fe93f109f13bb687065474472dce7752.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "512fc7a41b137f4e8117301a546ef0e7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e88074f4a406853802f3",
+    "url": "/static/css/2.488d03a5.chunk.css"
+  },
+  {
+    "revision": "e88074f4a406853802f3",
+    "url": "/static/js/2.4163561c.chunk.js"
+  },
+  {
+    "revision": "62cb41eaa9f6851928c1",
+    "url": "/static/js/main.8c9793d0.chunk.js"
+  },
+  {
+    "revision": "19be04129dd277f14c52",
+    "url": "/static/js/runtime~main.d8d359e6.js"
+  }
+]);
